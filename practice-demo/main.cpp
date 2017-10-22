706c143f6bf09fb520b4a3e1d3acad640862cd40
张琪祥 <zhangqixiang@meituan.com>
2017-10-22 23:01:10 +0800

    Initial Commit

---
+++ practice-demo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice-demo
+//
+//  Created by manatea on 2017/10/22.
+//  Copyright © 2017年 manatea. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
