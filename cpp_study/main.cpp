9c7461ce9845134919d9e95fcefcbc935c69e960
张琪祥 <zhangqixiang@meituan.com>
2017-10-22 23:27:08 +0800

    init

--- cpp_study/main.cpp
+++ cpp_study/main.cpp
@@ -7,9 +7,13 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int value,sum=0;
+    while(cin >> value)
+        sum += value;
+    cout << "sum is " << sum << endl;
     return 0;
 }
